af8cda73b05a6548b2edcd4da9c4efc8b8676f72
schatterjee6 <74698948+schatterjee6@users.noreply.github.com>
2021-01-20 09:08:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "sougata-rg"
-    storage_account_name = "sougata"
-    container_name       = "tfstate"
-    key                  = "newpoocterraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   version = "=2.33.0"
   features {}
