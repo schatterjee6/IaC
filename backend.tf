6bb3b6a5ffa61ca006190d4921fdfbfb5f35617a
schatterjee6 <74698948+schatterjee6@users.noreply.github.com>
2021-01-24 07:34:15 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "sougata-rg"
-    storage_account_name = "sougata"
-    container_name       = "tfstate"
-    key                  = "newpoocterraform.tfstate"
-  }
-}
