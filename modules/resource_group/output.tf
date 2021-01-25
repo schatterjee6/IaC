06e525d3fe0cd7d7d55e0f84fc2f45f9b9a3aae3
sougatac-admin <chatterjee.sougato@gmail.com>
2021-01-25 07:33:06 +0530

    Update output.tf

--- modules/resource_group/output.tf
+++ modules/resource_group/output.tf
@@ -1,12 +1,12 @@
 output rgname_Fw{
- value = element(concat(azurerm_resource_group.rg_arm.*.name, [""]), 0)
+ value = element(concat(azurerm_resource_group.arm_rg.*.name, [""]), 0)
 }
 output rgname_jmp{
- value = element(concat(azurerm_resource_group.rg_arm.*.name, [""]), 1)
+ value = element(concat(azurerm_resource_group.arm_rg.*.name, [""]), 1)
 }
 output rgname_nw{
- value = element(concat(azurerm_resource_group.rg_arm.*.name, [""]), 2)
+ value = element(concat(azurerm_resource_group.arm_rg.*.name, [""]), 2)
 }
 output rgname_web{
- value = element(concat(azurerm_resource_group.rg_arm.*.name, [""]), 3)
+ value = element(concat(azurerm_resource_group.arm_rg.*.name, [""]), 3)
 }
