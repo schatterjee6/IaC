3a21a043929710cfb8ffee68659a802842596b69
sougatac-admin <chatterjee.sougato@gmail.com>
2021-01-28 06:29:26 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     resource_group_name  = "sougata-rg"
     storage_account_name = "sougata"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "terraformnew.tfstate"
   }
 }
