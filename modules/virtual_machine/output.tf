f9a7d358f3ff19be95163d6a0cf7e671e914beb1
sougatac-admin <chatterjee.sougato@gmail.com>
2021-02-02 17:09:11 +0530

    Update output.tf

--- modules/virtual_machine/output.tf
+++ modules/virtual_machine/output.tf
@@ -6,10 +6,9 @@ output nic_id{
  value = azurerm_network_interface.vm.private_ip_address
 }
 
-output nsgname{
+/*output nsgname{
  value = azurerm_network_security_group.vm.name
-}
-
+}*/
 output "secret_value" {
   value = data.azurerm_key_vault_secret.secret.value
 }
